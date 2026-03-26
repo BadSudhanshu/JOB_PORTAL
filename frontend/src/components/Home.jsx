571e63d4f569f65715b5b983ed99a80a30aabf72
Rishikesh Kumar Sudhanshu <rishikeshsudhanshu@Rishikeshs-MacBook-Air.local>
2026-03-26 16:03:47 +0530

    navbar ui fixed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -20,6 +20,13 @@ const Home = () => {
   return (
     <div>
       <Navbar />
+      <div className='max-w-7xl mx-auto p-4'>
+        {user?.fullname && (
+          <h2 className='text-xl font-semibold mb-4'>
+            Welcome back, {user.fullname}!
+          </h2>
+        )}
+      </div>
       <HeroSection />
       <CategoryCarousel />
       <LatestJobs />
